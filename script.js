993c13735912db22d196bd012ad7638bccce8d94
ale9d8ce9 <ale9d8ce9@gmail.com>
2024-12-26 20:13:51 +0100

    now rectangles are aligned to the mouse location

--- script.js
+++ script.js
@@ -27,36 +27,41 @@ addSVG.startRect=function () {
     addSVG.vars={
         x:svg.getBoundingClientRect().x*-1,
         y:svg.getBoundingClientRect().y*-1,
-        w:0,
-        h:0
+        w:svg.getBoundingClientRect().x*-1,
+        h:svg.getBoundingClientRect().y*-1
     }
     i='drag.start();'
     i+='addSVG.vars.x+=window.event.clientX;'
     i+='addSVG.vars.y+=window.event.clientY;'
-    i+='console.log(addSVG.vars);'
     i+='this.setAttribute("onmousemove", "drag.move()")'
     body.setAttribute('onmousedown', i)
 
     i='this.removeAttribute("onmousedown");'
-    i+='addSVG.vars.w=window.event.clientX-addSVG.vars.x;'
-    i+='addSVG.vars.h=window.event.clientY-addSVG.vars.y;'
+    i+='addSVG.vars.w+=window.event.clientX-addSVG.vars.x;'
+    i+='addSVG.vars.h+=window.event.clientY-addSVG.vars.y;'
     i+='addSVG.createRect();'
     i+='this.removeAttribute("onmousemove");'
     i+='this.removeAttribute("onmouseup");'
     body.setAttribute('onmouseup', i)
 }
 addSVG.createRect=function () {
-    if (addSVG.vars.w<1) {
+    console.log(addSVG.vars)
+    addSVG.vars.x/=svg.getBoundingClientRect().width
+    addSVG.vars.y/=svg.getBoundingClientRect().height
+    addSVG.vars.w/=svg.getBoundingClientRect().width
+    addSVG.vars.h/=svg.getBoundingClientRect().height
+    if (addSVG.vars.w<0) {
         addSVG.vars.x+=addSVG.vars.w
         addSVG.vars.w*=-1
+        console.log(addSVG.vars);
+        
     }
-    if (addSVG.vars.h<1) {
+    if (addSVG.vars.h<0) {
         addSVG.vars.y+=addSVG.vars.h
         addSVG.vars.h*=-1
     }
-    addSVG.vars.x=addSVG.vars.x*window.innerWidth/svg.getBoundingClientRect().width
-    addSVG.vars.y=addSVG.vars.y*svg.getBoundingClientRect().height/window.innerHeight
-    svg.innerHTML+='<rect x="'+addSVG.vars.x+'" y="'+addSVG.vars.y+'" width="'+addSVG.vars.w+'" height="'+addSVG.vars.h+'" fill="none" stroke="#ff00ff" stroke-width="1" />'
+    console.log(addSVG.vars)
+    svg.innerHTML+='<rect x="'+addSVG.vars.x+'" y="'+addSVG.vars.y+'" width="'+addSVG.vars.w+'" height="'+addSVG.vars.h+'" fill="#00ffffa0" />'
 }
 
 
@@ -106,7 +111,7 @@ function resize(i,j) {
 
 
 function zoom(i) {
-    
+    svg.style.width=i+'px'
 }
 
 
